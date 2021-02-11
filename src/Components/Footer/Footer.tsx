9ef3ce5fb052efa744a59cf79803e2b5a78d18c1
laffed <roarklaffed@gmail.com>
2021-02-11 13:51:14 -0600

    fixed footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,22 +1,45 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFacebookF, faInstagram, faTwitter, faLinkedinIn, faYoutube} from '@fortawesome/free-brands-svg-icons';
+
 
 function Footer() {
 
   return (
-    <div className="footer-wrapper">
+    <footer>
       <div className="containerLarge dFlex">
         <div className="copy centerText">
           <p className="small">© 2021 Copyright CNC Catastrophe &amp; National Claims | <a href="https://adjustingexpectations.com">Visit our website</a>.</p>
         </div>
         <ul className="socialNav dFlex">
-          <li><a href="https://www.facebook.com/cnccatastrophe" target="_blank" rel='noreferrer'><i className="fab fa-facebook-f"></i></a></li>
-          <li><a href="https://www.instagram.com/cnccatastrophe/" target="_blank" rel='noreferrer'><i className="fab fa-instagram"></i></a></li>
-          <li><a href="https://twitter.com/cnccatastrophe" target="_blank" rel='noreferrer'><i className="fab fa-twitter"></i></a></li>
-          <li><a href="https://www.linkedin.com/company/cnccatastrophe/" target="_blank" rel='noreferrer'><i className="fab fa-linkedin-in"></i></a></li>
-          <li><a href="https://www.youtube.com/channel/UCbrRJiOjGx-3Us5iZhUuaTg" target="_blank" rel='noreferrer' ><i className="fab fa-youtube"></i></a></li>
+          <li>
+            <a href="https://www.facebook.com/cnccatastrophe" target="_blank" rel='noreferrer'>
+              <FontAwesomeIcon icon={faFacebookF} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/cnccatastrophe/" target="_blank" rel='noreferrer'>
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/cnccatastrophe" target="_blank" rel='noreferrer'>
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/company/cnccatastrophe/" target="_blank" rel='noreferrer'>
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/channel/UCbrRJiOjGx-3Us5iZhUuaTg" target="_blank" rel='noreferrer' >
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+          </li>
         </ul>
       </div>
-    </div>
+    </footer>
   );
 }
 
