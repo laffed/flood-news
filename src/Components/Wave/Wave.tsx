bc65e5b83968c9fb8c51189b3a86a6c5ea1788d7
laffed <roarklaffed@gmail.com>
2021-02-11 15:04:16 -0600

    Big Card rendering correctly

---
+++ src/Components/Wave/Wave.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Wave() {
+  return (
+    <div className="waveWrap">
+      <div className="wave"></div>
+      <div className="wave"></div>
+    </div>
+  )
+}
+
+export default Wave;
